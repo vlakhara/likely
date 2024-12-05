98ff74077c3d840f4daa24cad8f214487247acb4
vlakhara <vipullakhara18@gmail.com>
2024-12-06 00:50:55 +0530

    🎉FEAT: Added SignIn/SignUp Form and Google Auth Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Login from "./components/Login";
+
 const App = () => {
   return (
     <div className="App">
-      <div className="bg-gray-200 h-[100vh] w-[100vw] text-white text-center"></div>
+      <div className="bg-gray-900 h-[100vh] overflow-hidden w-[100vw] text-white text-center flex items-center justify-center">
+        <div className="bg-gray-200 h-full md:h-[90vh] w-full md:w-[450px] overflow-hidden rounded-none md:rounded-[12px] transition-all">
+          <Login />
+        </div>
+      </div>
     </div>
   );
 };
